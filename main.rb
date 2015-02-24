460bd89f4cdda1df8cf1a949e43c4f8337bc5fe7
eric-irel <eric.herskovic@gmail.com>
2015-02-24 14:16:05 -0500

    Scraped cover art, title, genre, score and description. Added Bootstrap.

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
 require 'sinatra'
 require 'nokogiri'
 require 'open-uri'
+
+get '/' do
+  url = "http://www.ign.com/games/reviews"
+  data = Nokogiri::HTML(open(url))
+  @games = data.css('.itemList-item')
+  erb :reviews
+end
